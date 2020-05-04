9525ffed232124566fcbe866bfcb0ec2ba804f2a
madenney <matt.a.denney@gmail.com>
2020-05-04 10:05:25 -0700

    fixes all the stuff you have been complaining about

---
+++ constants/comboFilterDefaults.js
@@ -0,0 +1,11 @@
+const defaults = {
+    comboer: "",
+    comboee: "",
+    stage: "",
+    minMoves: 8,
+    minDamage: 60,
+    endMove: "",
+    didKill: false
+}
+
+module.exports = { defaults };
--- models/ComboList.js
+++ models/ComboList.js
@@ -1,7 +1,18 @@
 
 const slpToVideo = require("slp-to-video");
 const fs = require("fs");
-const OUTPUT_PATH = "/Users/mattdenney/Projects/Archiver/airlock/output.avi"
+const events = require("events");
+const path = require("path");
+
+const config = require("../config.json");
+
+
+const VIDEO_OUTPUT_PATH = path.resolve(path.join(config.AIRLOCK_PATH,"output.avi"));
+const REPLAYS_JSON_PATH = path.resolve(path.join(config.AIRLOCK_PATH,"replays.json"));
+const SSBM_ISO_PATH = path.resolve(config.SSBM_ISO_PATH);
+const DOLPHIN_PATH = path.resolve("./node_modules/slp-to-video/Ishiiruka/build/Binaries/dolphin-emu");
+const NUM_PROCESSES = 8;
+
 class ComboList {
 
     constructor(combos){
@@ -11,38 +22,60 @@ class ComboList {
             this.combos = [];
         }
 
+        if(!fs.existsSync(config.SSBM_ISO_PATH)){
+            throw "HEY, YOUR SSBM_ISO_PATH is invalid. Check config.json";
+        }
+        if(!fs.existsSync(config.AIRLOCK_PATH)){
+            throw "HEY, YOUR AIRLOCK_PATH is invalid. Check config.json";
+        }
+        if(!fs.existsSync(DOLPHIN_PATH)){
+            throw "HEY, I couldn't find dolphin-emu in your slp-to-video module. Did you run './setup.sh'?";
+        }
+
     }
 
     generateVideo(){
         return new Promise( async (resolve,reject) => {
-            
-            const json = [
-                {
-                    "output_path": OUTPUT_PATH,
-                    "replays": []
-                }
-            ]
-            
+
+            console.log(`Generating videos using ${NUM_PROCESSES} cpus`);
+            const json = [{"output_path": VIDEO_OUTPUT_PATH,"replays": []}]
             this.combos.forEach(combo => {
+                //imagePath = createImage(combo);
                 json[0].replays.push({
                     replay: combo.game.slpPath,
                     startFrame: combo.combo.startFrame,
-                    endFrame: combo.combo.endFrame
+                    endFrame: combo.combo.endFrame,
+                    //overlay: imagePath
                 })
             });
-
-            fs.writeFileSync("/Users/mattdenney/Projects/Archiver/airlock/replays.json",JSON.stringify(json));
+            fs.writeFileSync(REPLAYS_JSON_PATH,JSON.stringify(json));
+            const em = new events.EventEmitter();
             const config = {
-                inputFile: "/Users/mattdenney/Projects/Archiver/airlock/replays.json",
-                dolphinPath: "/Users/mattdenney/Projects/Archiver/node_modules/slp-to-video/Ishiiruka/build/Binaries/dolphin-emu",
-                ssbmIsoPath: "/Users/mattdenney/Projects/melee.iso",
-                numCPUs: 2
+                INPUT_FILE: REPLAYS_JSON_PATH,
+                DOLPHIN_PATH,
+                SSBM_ISO_PATH,
+                NUM_PROCESSES,
+                EVENT_TRACKER: em
             }
+            em.on('primaryEventMsg',msg => {
+                console.log(msg);
+            });
+            const totalVideos = this.combos.length;
+            em.on('count', count => {
+                console.log(`${count}/${totalVideos}`);
+            });
+            const skippedFiles = [];
+            em.on('errorEventMsg',(msg,file) => {
+                console.log(msg);
+                skippedFiles.push(file);
+            })
             try {
                 await slpToVideo(config);
+                console.log("Skipped Files: ", skippedFiles.length, skippedFiles );
                 resolve();
+
             } catch(err){
-                console.log("Error occurred while generating video:");
+                console.log("Error occurred in slp-to-video");
                 reject(err);
             }
             
