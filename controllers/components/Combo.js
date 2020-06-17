0481b9cc29c4c7c647d4f3a9463e417a798ffe2f
madenney <matt.a.denney@gmail.com>
2020-06-16 18:44:20 -0700

    adds total combo time

--- controllers/components/Combo.js
+++ controllers/components/Combo.js
@@ -5,7 +5,8 @@ const { stages } = require("../../constants/stages");
 const darkStages = [2,3,31,32];
 
 class ComboController {
-    constructor(combo){
+    constructor(combo,id){
+        this.id = id
         this.combo = combo;
         this.slpPath = combo.slpPath;
         this.comboer = combo.players.find(p => {
@@ -14,14 +15,15 @@ class ComboController {
         this.comboee = combo.players.find(p => {
             return p.playerIndex === combo.opponentIndex
         });
+        this.isSelected = false;
     }
 
     html(){
-        const { combo, comboer, comboee, slpPath } = this;
+        const { combo, comboer, comboee, slpPath, id } = this;
         const damage = Math.floor(combo.endPercent - combo.startPercent);
         const seconds = (( combo.endFrame - combo.startFrame ) / 60).toFixed(1);
         return $(`
-        <div id="${combo.id}" class="combo">
+        <div id="${id}" class="combo">
             <div class='game-info'>
                 <div class='characters'>
                     <img class='char1' src=${characters[comboer.characterId].img}>
@@ -48,6 +50,8 @@ class ComboController {
                     <div class='data'>${slpPath}</div>
                 </div>
             </div>
+            <input class="combo-checkbox" type="checkbox">
+            <div class="included-indicator"></div>
         </div>
         `);
     }
