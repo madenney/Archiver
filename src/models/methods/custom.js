802ca3d3594b270500fdf4bfd46ba51c38a5ee42
madenney <matt.a.denney@gmail.com>
2022-11-25 16:04:59 -0800

    adds custom search for thunder's combo

--- src/models/methods/custom.js
+++ src/models/methods/custom.js
@@ -4,6 +4,15 @@ export default (prev, params, eventEmitter) => {
         const {} = params
         const { moves, comboer, comboee, path, stage } = combo
 
+        // find thunder's combo
+        const thunders = moves.find((move, index) => {
+            if( moves[index+1] && moves[index + 2]){
+                if( move.moveId == 21 && moves[index+1].moveId == 2 &&
+                (moves[index+2].moveId == 16 || moves[index+2].moveId == 11)
+                    ) return true
+            }
+        })
+        if(!thunders) return false
         
 
         return {
